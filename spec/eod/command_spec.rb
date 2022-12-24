2508cbadeb7c345df797fbd4d73d1ea7625c751f
Danny Ben Shitrit <db@dannyben.com>
2022-12-24 11:23:22 +0000

    housekeeping

--- spec/eod/command_spec.rb
+++ spec/eod/command_spec.rb
@@ -2,37 +2,38 @@ require 'spec_helper'
 
 describe Command do
   subject { CLI.runner }
+
   before { require_mock_server! }
 
   # Test all commands as defined in the spec config
   config = YAML.load_file 'spec/eod/commands.yml'
   config.each do |name, command|
     context name do
-      it "works" do
-        args = command.is_a?(Array) ? command : command.split(' ')
+      it 'executes successfully' do
+        args = command.is_a?(Array) ? command : command.split
         expect { subject.run args }.to output_approval("cli/#{name}")
       end
     end
   end
 
   # Test other non-standard cases
-  context "calendar command with an invalid calendar" do
-    it "raises a friendly error" do
-      expect { subject.run %w[calendar holidays] }.to raise_approval("cli/calendar-invalid")
+  context 'with calendar command and an invalid calendar' do
+    it 'raises a friendly error' do
+      expect { subject.run %w[calendar holidays] }.to raise_approval('cli/calendar-invalid')
     end
   end
 
-  context "any command with an invalid --format flag" do
-    it "raises a friendly error" do
-      expect { subject.run %w[data SYMBOL --format xml] }.to raise_approval("cli/data-invalid-format")
+  context 'with any command and an invalid --format flag' do
+    it 'raises a friendly error' do
+      expect { subject.run %w[data SYMBOL --format xml] }.to raise_approval('cli/data-invalid-format')
     end
   end
 
-  context "any command with --save" do
+  context 'with any command and --save' do
     let(:filename) { 'spec/tmp/out.json' }
 
-    it "saves the output to a file" do
-      expect { subject.run %W[data SYMBOL --save #{filename}] }.to output_approval("cli/data-save")
+    it 'saves the output to a file' do
+      expect { subject.run %W[data SYMBOL --save #{filename}] }.to output_approval('cli/data-save')
       expect(File).to exist(filename)
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,27 +9,28 @@ require 'eod/cli'
 include EOD
 
 FAKE_API_TOKEN = 'fake-test-token'
+PRODUCTION_API_BASE = API.base_uri
+TEST_API_BASE = 'http://localhost:3000/'
 ENV['EOD_CACHE_DIR'] = 'spec/cache'
 ENV['EOD_API_TOKEN'] = FAKE_API_TOKEN
 
 def require_mock_server!
   result = HTTParty.get('http://localhost:3000/')
   result = JSON.parse result.body
-  raise "Please start the mock server" unless result['mockserver'] == 'online'
+  raise 'Please start the mock server' unless result['mockserver'] == 'online'
 rescue Errno::ECONNREFUSED
   # :nocov:
-  raise "Please start the mock server"
+  raise 'Please start the mock server'
   # :nocov:
 end
 
 RSpec.configure do |c|
   c.filter_run_excluding :require_test_api_token unless ENV['EOD_TEST_API_TOKEN']
+  c.example_status_persistence_file_path = 'spec/status.txt'
 
   c.before :suite do
-    PRODUCTION_API_BASE = API.base_uri
-    TEST_API_BASE = "http://localhost:3000/"
     API.base_uri TEST_API_BASE
-    
+
     system 'mkdir -p spec/tmp && rm -rf spec/cache'
   end
 end
